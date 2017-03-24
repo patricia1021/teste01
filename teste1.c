e3ea12ed40df989cb39252feca2ddb6e7c229364
Patrícia Guimarães <deud.patricia@yahoo.com.br>
2017-03-24 15:47:28 -0300

    teste
    01

---
+++ teste1.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	char a[10];
+
+	for (int i = 0; i < 10; i++)
+	{
+		a[i]= "a";
+		printf("%c\n", a[i]);
+	}
+
+
+
+
+/*	printf("Digite um valor \n");
+	scanf("%d", &a);
+
+	printf("Digite um valor \n");
+	scanf("%d", &b);
+
+	printf("Digite um valor \n");
+	scanf("%d", &c);*/
+
+/*	switch (c) {
+		case 1: 
+			c = a + b;
+			break;
+
+		case 2:
+			c = a - b;
+			break;
+
+		case 3:
+			c = a * b;
+			break;
+
+		case 4:
+			c = a / b;
+			break;
+	}*/
+
+//	int t1= a;
+//	for (int i = 0; i < b; i++) a=t1*a;
+/*
+	while (c < 10) 
+		{
+			c++;
+			printf("%d\n", c);
+		}
+
+	printf("Eu sou uma gênia.\n");*/
+
+
+	/*if (c > 10)
+	{
+		printf("O Gabriel é inteligente.\n");
+	}
+
+	else printf("Eu sou inteligente.\n");*/
+
+	//printf("O valor de c eh %d \n", c);
+
+	return 0;
+}
